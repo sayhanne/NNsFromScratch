262d7fe8fbb75b690c371150c7e0aa5cc161cf73
sayhanne <sayhannee@gmail.com>
2022-10-25 13:45:26 +0300

    Categorical cross-entropy implemented

--- ObjectLayers.py
+++ ObjectLayers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 np.random.seed(0)
 
@@ -29,7 +30,7 @@ class Layer_Dense:
 
     def __init__(self, n_inputs, n_neurons):
         self.output = None
-        self.weights = 0.10 * np.random.randn(n_neurons, n_inputs)
+        self.weights = 0.01 * np.random.randn(n_neurons, n_inputs)
         self.biases = np.zeros(shape=(1, n_neurons))
         # We can define weights the other way to get rid of the transpose operation
         # in the forward function
@@ -62,6 +63,35 @@ class Activation_Softmax:
         self.output = probabilities
 
 
+class Loss:
+    def calculate(self, output, target):
+        sample_losses = self.forward(output, target)
+        cost = np.mean(sample_losses)
+        return cost
+
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_target):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # first check the output format
+        # whether if it's one-hot vector or just a scalar value
+        # one-hot vector -> a vector that has 1 in the index of the correct class, others are 0.
+        # scalar value -> indicating the correct class
+
+        if len(y_target.shape) == 1:  # scalar valued outputs
+            # Getting the value of our prediction which corresponds to our target class index
+            correct_confidences = y_pred_clipped[range(samples), y_target]
+        elif len(y_target.shape) == 2:  # one-hot vector
+            # Take dot product of the predicted outputs(confidences) and target values(one-hot vector).
+            # Then take the sum of each row to put get the input which we will put to log function later.
+            correct_confidences = np.sum(y_pred_clipped * y_target, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
 if __name__ == '__main__':
     X, y = create_data(100, 3)
 
@@ -81,8 +111,11 @@ if __name__ == '__main__':
     layer1.forward(X)  # z = wT*x + b
     activation1.forward(layer1.output)  # y = ReLU(z)
 
-    layer2.forward(activation1.output)  # z = wT*x + b   x -> layer1 output
+    layer2.forward(activation1.output)  # z = wT*x + b   x -> activation1 output
     activation2.forward(layer2.output)  # y = softmax(z)
 
     print(activation2.output[:5])
 
+    loss_function = Loss_CategoricalCrossEntropy()
+    loss = loss_function.calculate(activation2.output, y)
+    print("Loss: ", loss)
